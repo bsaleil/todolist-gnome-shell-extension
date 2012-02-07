56527bc1bc0f7b1bb40f64f56eb30d614eca8518
A. B <arnaud.bonatti@gmail.com>
2012-02-08 00:30:04 +0100

    oups…

--- extension.js
+++ extension.js
@@ -93,7 +93,7 @@ TasksManager.prototype =
 		this.newTask = new St.Entry(
 		{
 			name: "searchEntry",
-			hint_text: Gettext.gettext("New task…"),
+			hint_text: Gettext.gettext("New task..."),
 			track_hover: true,
 			can_focus: true
 		});
